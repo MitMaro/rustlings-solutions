82e6d4f6d99ca311dd4c2b68df3bf83bd0ca3cea
Tim Oram <dev@mitmaro.ca>
2018-03-04 13:30:34 -0330

    Allow test script to run a single file

--- test.bash
+++ test.bash
@@ -2,10 +2,17 @@
 
 out_directory="out/"
 
-srcs="$(cd lessons && echo **/*.rs)"
+if [[ -z "$1" ]]; then
+	srcs="$(cd lessons && echo **/*.rs)"
+else
+	srcs="${1/#lessons\///}"
+fi
 
 for f in $srcs; do
+	echo "Compiling lessons/$f"
 	rustc --out-dir "${out_directory}$(dirname $f)/" "lessons/$f"
+	echo "Running lessons/$f"
 	"./${out_directory}${f%.*}"
+	echo ""
 done
 
